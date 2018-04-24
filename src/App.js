ec191d379f6772834debc80911167ba9bebd14ad
Jeff Thomas <jathom30@gmail.com>
2018-04-24 09:26:17 -0400

    initial init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Main from './components/Main'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// export default class Main extends Component {
+//   render() {
+//     return(
+//       <div className="main">
+//         This is the main div
+//       </div>
+//     )
+//   }
+// }
+
+const Main = (props) => {
+  return (
+    <div className="main">
+      <div className="background">
+        building background
+      </div>
+      <div id="too-big-to-fail">
+        album cover
+      </div>
+    </div>
+  )
+}
+
+export default Main
