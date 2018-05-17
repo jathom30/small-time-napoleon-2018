6b2c785c86cd2c850227e75b1f3ea73406846547
Jeff Thomas <jathom30@gmail.com>
2018-05-17 12:15:51 -0400

    image data file containing photo album sources and alts

---
+++ src/data/imageData.js
@@ -0,0 +1,65 @@
+import dan from '../assets/photoSection/dan.jpg'
+import danSings from '../assets/photoSection/danSings.jpg'
+import daveJeff from '../assets/photoSection/daveJeff.jpg'
+import jeffSings from '../assets/photoSection/jeffSings.jpg'
+import newTrio from '../assets/photoSection/newTrio.jpg'
+import oldQuartet from '../assets/photoSection/oldQuartet.jpg'
+import trio from '../assets/photoSection/trio.jpg'
+import wedding1 from '../assets/photoSection/wedding1.jpg'
+import wedding2 from '../assets/photoSection/wedding2.jpg'
+import wedding3 from '../assets/photoSection/wedding3.jpg'
+import wedding4 from '../assets/photoSection/wedding4.jpg'
+import zack from '../assets/photoSection/zack.jpg'
+
+const photosObject = [
+  {
+    source: daveJeff,
+    alt: 'Dave and Jeff 2017 at "Warm up Louisville"',
+  },
+  {
+    source: wedding3,
+    alt: 'Jeff 2017 at an outdoor wedding',
+  },
+  {
+    source: oldQuartet,
+    alt: 'The band 2015 at the Clifton Center',
+  },
+  {
+    source: danSings,
+    alt: 'Dan 2017 at "Warm up Louisville"',
+  },
+  {
+    source: wedding2,
+    alt: 'The band 2017 at an outdoor wedding',
+  },
+  {
+    source: jeffSings,
+    alt: 'Jeff 2017 at "Warm up Louisville"',
+  },
+  {
+    source: wedding4,
+    alt: 'The band 2017 at an outdoor wedding',
+  },
+  {
+    source: trio,
+    alt: 'The band 2015 at the Clifton Center',
+  },
+  {
+    source: zack,
+    alt: 'Zack 2017 at "Warm up Louisville"',
+  },
+  {
+    source: newTrio,
+    alt: 'The band 2017 at "Warm up Louisville"',
+  },
+  {
+    source: wedding1,
+    alt: 'The band 2017 at an outdoor wedding',
+  },
+  {
+    source: dan,
+    alt: 'Dan 2017 at "Warm up Louisville"',
+  },
+]
+
+export default photosObject
