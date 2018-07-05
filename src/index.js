b9589e1e19d40b48938870647c75870592f4d0c7
Jeff Thomas <jathom30@gmail.com>
2018-07-05 13:04:49 -0400

    adding netlify indentify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import netlifyIdentity from "netlify-identity-widget";
 
+let buttons = document.createElement('div')
+netlifyIdentity.netlifyIdentity()
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
